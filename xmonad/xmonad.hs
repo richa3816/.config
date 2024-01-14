551a4243a8460e95e7e5603079947c5036d581eb
richa <130410080+richa3816@users.noreply.github.com>
2024-01-14 13:18:32 +0000

    Updated bow config

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,24 +1,30 @@
 import XMonad
 import XMonad.Util.EZConfig (additionalKeysP)
 import XMonad.Hooks.DynamicLog (xmobarProp)
+import XMonad.Layout.NoBorders
+import XMonad.Hooks.ManageDocks
+import XMonad.Layout.Spacing
 
 myTerminal		= "kitty"
-myModMask		= mod1Mask
+myModMask		= mod4Mask
 myFocusFollowsMouse	:: Bool
 myFocusFollowsMouse	= False
 myClickJustFocuses	:: Bool
 myClickJustFocuses	= True
-myNormalBorderColor	= "#000000"
-myFocusedBorderColor	= "#aaaaaa"
-myLayout		= tiled ||| Full
-  where
-    tiled	= Tall nmaster delta ratio
-    nmaster	= 1	-- Default number of windows in the master pane
-    ratio	= 1/2	-- Default proportion of screen occupied by master pane
-    delta	= 3/100	-- Percent of screent o increment by when resizing panes
+myNormalBorderColor	= "#333333"
+myFocusedBorderColor	= "#FFFFFF"
+myLayout		= smartBorders
+			  $ avoidStruts
+			  $ spacingRaw False (Border 20 0 20 0) False (Border 0 20 0 20) False
+			  $ tiled ||| Full
+			  where
+			    tiled	= Tall nmaster delta ratio
+			    nmaster	= 1	-- Default number of windows in the master pane
+			    ratio	= 1/2	-- Default proportion of screen occupied by master pane
+			    delta	= 3/100	-- Percent of screen to increment by when resizing panes
 
 main :: IO ()
-main = xmonad $ xmobarProp $ def
+main = xmonad $ xmobarProp $ docks def
   { terminal		= myTerminal
   , modMask		= myModMask
   , focusFollowsMouse	= myFocusFollowsMouse
@@ -28,6 +34,7 @@ main = xmonad $ xmobarProp $ def
   , layoutHook		= myLayout
   }
   `additionalKeysP`
-    [ ("M-p", spawn "dmenu_run -fn 'FreeMono-11'")
+    [ ("M-p", spawn "dmenu_run -fn 'FreeMono-11' -nb '#000000' -nf '#777777' -sb '#000000' -sf '#FFFFFF'")
     , ("<Print>", spawn "flameshot gui")
+    , ("M-S-g", sequence_ [toggleScreenSpacingEnabled, toggleWindowSpacingEnabled])
     ]
