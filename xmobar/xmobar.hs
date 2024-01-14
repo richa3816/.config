494025fe68dbb5bb442e79c478c97fa25bc9e27b
richa <130410080+richa3816@users.noreply.github.com>
2024-01-14 13:20:18 +0000

    Updated to left-align datetime and improve readability

--- xmobar/xmobar.hs
+++ xmobar/xmobar.hs
@@ -4,17 +4,31 @@ Config { overrideRedirect = False
   , bgColor		= "#000000"
   , fgColor		= "#FFFFFF"
   , position		= TopW L 100
-  , commands		= [ Run Date "%Y-%m-%d %H:%M" "date" 10
+  , commands		= [ Run Date "%F %T" "date" 10
   			  , Run Cpu
-			    [ "-L"		, "3"
-			    , "-H"		, "50"
-			    , "--high"		, "red"
-			    , "--normal"	, "green"
+			    [ "--template"	, "CPU <total>%"
+			    , "--Low"		, "3"
+			    , "--High"		, "50"
+			    , "--low"		, "#CAD3F5"
+			    , "--normal"	, "#CAD3F5"
+			    , "--high"		, "#ED8796"
 			    ] 10
-			  , Run Memory ["--template", "RAM: <usedratio>%" ] 10
-			  , Run Swap [] 10
+			  , Run Memory ["--template", "RAM <usedratio>%" ] 10
+			  , Run Swap ["--template",	"SWAP <usedratio>%"] 10
+			  , Run Battery
+			    [ "--template"	, "BAT <acstatus>"
+			    , "--Low"		, "20"
+			    , "--High"		, "80"
+			    , "--low"		, "#ED8796"
+			    , "--normal"	, "#CAD3F5"
+			    , "--high"		, "#A6DA95"
+			    , "--"
+			      , "-o"		, "<left>%-"
+			      , "-O"		, "<left>%+"
+			      , "-i"		, "<left>%p"
+			    ] 50
 			  ]
   , sepChar		= "%"
   , alignSep		= "}{"
-  , template		= "} %date% { %cpu% | %memory% | %swap% "
+  , template		= " %date% } { %cpu% %memory% %swap% %battery% "
 }
